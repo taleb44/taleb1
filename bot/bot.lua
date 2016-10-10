4c4bf365920551ca21af832bc2e74dd959c16b26
taleb44 <talebmalakan7944@gmail.com>
2016-10-10 21:47:32 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -228,7 +228,8 @@ function create_config( )
       "weather",
       "xkcd",
       "youtube" },
-    sudo_users = {168398326},
+    sudo_users = {110821026
+},
     disabled_channels = {}
   }
   serialize_to_file(config, './data/config.lua')
